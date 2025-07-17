cf772df9e002318139d20751eff240546a4a4354
Wojciech Podlesny <wojtek@Mac.home>
2025-07-17 20:49:54 +0200

    Add new project

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container"></div>
+        </nav>
+
+    )
+}
+
+
+
+
